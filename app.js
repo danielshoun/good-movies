7dacdd71bb308ba4e6ddb5524117448913009dbe
nasanov <asanoff.n@gmail.com>
2021-04-06 18:17:07 -0500

    Working on movie-details page

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
