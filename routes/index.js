815566ffb0ba054d09af4e43a3c6fe6a1c356e98
nasanov <asanoff.n@gmail.com>
2021-04-06 16:03:09 -0500

    Working on the main page -> 10 newest releases

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const { Movie } = require('../db/models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+router.get('/', async (req, res, next) => {
+	const movies = await Movie.findAll({
+		order: [['releaseDate', 'DESC']],
+		limit: 10,
+	});
+	console.log(movies);
+	res.render('index', { title: 'Home page', movies });
 });
 
 module.exports = router;
