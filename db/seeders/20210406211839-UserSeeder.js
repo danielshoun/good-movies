7f1a38132609dcbaddd14fa23638739bd9979e45
John-Witter <mrsolginski@gmail.com>
2021-04-06 17:47:02 -0400

    seeding reviews

---
+++ db/seeders/20210406211839-UserSeeder.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert('Users', [
+      {
+        username: 'demo',
+        email: "demo@demo.com",
+        hashedPassword: "demopassworD1!",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Users', null, {});
+  }
+};
---
+++ db/seeders/20210406213849-ReviewSeeder.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert('Reviews', [
+      {
+        userId: 1,
+        movieId: 1,
+        reviewText: "I liked it!!",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 1,
+        movieId: 2,
+        reviewText: "I liked it!!",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 1,
+        movieId: 3,
+        reviewText: "I liked it!!",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 1,
+        movieId: 4,
+        reviewText: "I liked it!!",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Reviews', null, {});
+  }
+};
