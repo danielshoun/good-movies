5292c8156dc4e2d95d4d1da61556c37f549b6fd7
nasanov <asanoff.n@gmail.com>
2021-04-07 12:35:48 -0500

    Working on the reviews

--- routes/movies.js
+++ routes/movies.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const { Movie } = require('../db/models');
+const { Movie, Review, User } = require('../db/models');
 const { asyncHandler, handleValidationErrors } = require('../utils');
 
 router.get('/', function (req, res, next) {
 	res.send('respond with a resource');
 });
 
-router.get('/:id(\\d+)', asyncHandler( async(req, res, next) => {
-	const movieId = parseInt(req.params.id, 10);
+router.get(
+	'/:id(\\d+)',
+	asyncHandler(async (req, res, next) => {
+		const movieId = parseInt(req.params.id, 10);
 
-	const movie = await Movie.findByPk(movieId);
-	movie.genres = movie.genres.join(", ")
-	movie.cast = movie.cast.join(", ")
+		const movie = await Movie.findByPk(movieId);
+		movie.genres = movie.genres.join(', ');
+		movie.cast = movie.cast.join(', ');
 
-	res.render('movie-details', { movie, title: 'Movie Details'});
-}));
+		const reviews = await Review.findAll({
+			where: {
+				movieId: movieId,
+			},
+			include: [User],
+		});
+
+		res.render('movie-details', { movie, reviews, title: 'Movie Details' });
+	})
+);
 
 module.exports = router;
