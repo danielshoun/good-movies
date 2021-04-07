fff28bed26257a1a84e3c65412305ce6e5aca165
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-04-07 13:14:02 -0400

    "All Lists" and individual movie lists pages functioning; working on styling them.

--- routes/movie-lists.js
+++ routes/movie-lists.js
@@ -5,21 +5,38 @@ const { asyncHandler } = require('../utils')
 const { restoreUser } = require('../auth')
 
 router.get('/', restoreUser, asyncHandler(async (req, res, next) => {
-    //get each of user's lists
-    //render every movie from each of their lists
-    const currentUserId = res.locals.user.id
-    
-    console.log(currentUserId)
+    if(res.locals.authenticated) {
+        //get each of user's lists
+        //render every movie from each of their lists
+        const currentUserId = res.locals.user.id
 
-    const movieLists = await MovieList.findAll({ 
-        where: { userId: currentUserId }, 
-        include: Movie
-    })
-    // const moviesAndLists = await MoviesAndLists.findAll({ where: { movieListsId: movieLists.id } })
+        console.log(currentUserId)
 
-    console.log('movieLists:', movieLists)
+        const movieLists = await MovieList.findAll({
+            where: { userId: currentUserId },
+            include: Movie
+        })
+        // const moviesAndLists = await MoviesAndLists.findAll({ where: { movieListsId: movieLists.id } })
 
-    res.render('movieList', {title: 'Movie Lists', movieLists})
+        console.log('movieLists:', movieLists)
+
+        res.render('movieList', {title: 'Movie Lists', movieLists})
+    } else {
+        res.redirect('/users/login')
+    }
+
+}))
+
+router.get('/:id(\\d+)', restoreUser, asyncHandler(async (req, res, next) => {
+    if(res.locals.authenticated) {
+        const currentUserId = res.locals.user.id;
+        const movieListId = req.params.id;
+
+        const currentMovieList = await MovieList.findByPk(movieListId, {include: Movie});
+        const movieLists = await MovieList.findAll({where: {userId: currentUserId}});
+
+        res.render('movieList', {title: 'Movie Lists', currentMovieList, movieLists});
+    }
 }))
 
 module.exports = router
